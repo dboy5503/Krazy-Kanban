3caee0300222b4fda3c44dbffd45c5c4ec069d02
DOni5503 <dboyer5503@gmail.com>
2024-10-08 19:56:14 -0400

    updated env, completed authenticateToken, completed login method

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -2,9 +2,30 @@ import { Router, Request, Response } from 'express';
 import { User } from '../models/user.js';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
+import { json } from 'sequelize';
 
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
+  const { username, password } = req.body;
+
+  const user = await User.findOne({
+    where: {
+      username
+    }
+  });
+  if (!user) {
+    return res.sendStatus(401),json({message: 'Authentication failed'});
+  }
+
+  const passwordMatch = await bcrypt.compare(password, user.password);
+  if (!passwordMatch) {
+    return res.sendStatus(401),json({message: 'Authentication failed'});
+  }
+
+  const secretKey = process.env.JWT_SECRET_KEY || 'AAAAAAAAAAAAA';
+
+  const token = jwt.sign({ username }, secretKey, { expiresIn: '3h' });
+  return res.json({ token });
 };
 
 const router = Router();
